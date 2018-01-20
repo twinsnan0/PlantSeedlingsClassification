1719d3a4998eabaee1eee97f658898422568450a
huchengsong <huchengsong0814@gmail.com>
2018-01-20 13:56:15 -0500

    imporved download.py

--- download.py
+++ download.py
@@ -1,6 +1,4 @@
-import json
 import os
-
 from kaggle_data.downloader import KaggleDataDownloader
 
 import constants
@@ -8,41 +6,33 @@ import constants
 # To run this code please install the Kaggle-data-downloader
 # pip install -U git+https://github.com/EKami/kaggle-data-downloader.git
 
-def download(competition_name, data_file_name, directory=None, file_name="test"):
+
+def download(user_pw, competition_name, data_file_name, directory=None):
     if directory is None:
         directory = os.getcwd()
 
-    file_path = os.path.join(directory, file_name)
-    if not os.path.exists(file_path):
-        os.makedirs(file_path)
-    print(os.path.abspath(file_path))
-
     # We can not download the data without user info
-    username, password = get_user_info()
-    print("username:{}, password:{}".format(username, password))
-    downloader = KaggleDataDownloader(username, password, competition_name)
+    downloader = KaggleDataDownloader(user_pw[0], user_pw[1], competition_name)
 
     download_file_path = os.path.join(directory, data_file_name)
-
     if os.path.exists(download_file_path):
         print("Data exists")
     else:
         downloader.download_dataset(data_file_name, directory)
 
-    downloader.decompress(download_file_path, file_path)
+    downloader.decompress(download_file_path, directory)
 
 
 def get_user_info() -> (str, str):
-    # Warning: you can not push the user_info.json file to github
-    with open('user_info.json') as json_file:
-        data = json.load(json_file)
-        return data["username"], data["password"]
+    # require user's input
+    username = input("Please enter username for Kaggle: ")
+    password = input("Password: ")
+    return username, password
 
 
 if __name__ == "__main__":
     # Download and decompress data set
-
-    download(constants.train_data_name, "sample_submission.csv.zip", r"../PlantSeedlingsImage/",
-             "sample_submission.csv")
-    download(constants.train_data_name, "test.zip", r"../PlantSeedlingsImage/", "test")
-    # download(constants.train_data_name, "train.zip", r"../PlantSeedlingsImage/", "train")
+    user_info = get_user_info()
+    #download(user_info, constants.train_data_name, "sample_submission.csv.zip", r"../PlantSeedlingsImage/","sample_submission.csv")
+    #download(user_info, constants.train_data_name, "test.zip", r"../PlantSeedlingsImage/", "test")
+    download(user_info, constants.train_data_name, "train.zip", r"../PlantSeedlingsImage/")
