c8bddcf704f62ca339dfcd60a640e56254fc05a0
twinsnan0 <twinsnan@foxmail.com>
2018-01-26 23:01:33 +0800

    add volatile=True when test and validation

--- train.py
+++ train.py
@@ -31,13 +31,15 @@ def train(save_directory: str, model_path: str = None):
     if model_path is not None:
         load_model(net, model_path)
 
-    for epoch in range(0, 0):
+    for epoch in range(0, 1):
         train_epoch(net, data, epoch, normalize)
         accuracy = validate_epoch(net, data, epoch, normalize)
         accuracy_list.append(accuracy)
         save_model(net, save_directory, accuracy=accuracy)
         if accuracy_list.index(max(accuracy_list)) == len(accuracy_list) - 1:
             save_model(net, save_directory, is_best=True)
+
+    del net
 
 
 def test(model_path: str = None):
@@ -58,15 +60,18 @@ def test(model_path: str = None):
     with open("submission.txt", "a") as submission_file:
         for test_image_dir, test_image in data.generate_test_data():
             test_tensor = normalize(torch.from_numpy(test_image))
-            test_x = Variable(test_tensor).cuda().float()
+            test_x = Variable(test_tensor, volatile=True).cuda().float()
             test_output = net(test_x)
             _, predict_y = torch.max(test_output, 1)
             print("Predict result:")
             print(predict_y)
-            file_name = os.path.split(test_image_dir)[1].split("_")[0]+'.'+os.path.split(test_image_dir)[1].split('.')[1]
+            file_name = os.path.split(test_image_dir)[1].split("_")[0] + '.' + \
+                        os.path.split(test_image_dir)[1].split('.')[1]
             string_to_write = "{},{}\r\n".format(file_name, SeedlingsData.seedlings_labels[predict_y.data[0]])
             submission_file.write(string_to_write)
             print(string_to_write)
+
+    del net
 
 
 def create_submission_file(filename):
@@ -103,8 +108,8 @@ def validate_epoch(net: Net, data: SeedlingsData, epoch: int, normalize: transfo
     validate_right = 0
     for validate_batch_index, validate_images, validate_labels in data.generate_validate_data():
         validate_tensor = normalize(torch.from_numpy(validate_images))
-        validate_batch_x = Variable(validate_tensor).cuda().float()
-        validate_batch_y = Variable(torch.from_numpy(validate_labels)).cuda().long()
+        validate_batch_x = Variable(validate_tensor, volatile=True).cuda().float()
+        validate_batch_y = Variable(torch.from_numpy(validate_labels), volatile=True).cuda().long()
         validate_output = net(validate_batch_x)
         _, predict_batch_y = torch.max(validate_output, 1)
         validate_total += validate_batch_y.size(0)
@@ -112,6 +117,7 @@ def validate_epoch(net: Net, data: SeedlingsData, epoch: int, normalize: transfo
         accuracy = validate_right / validate_total
         print("Epoch:{} ,validate_batch index:{}, validate_accuracy:{}".format(epoch, validate_batch_index,
                                                                                accuracy))
+    net.eval()
 
     accuracy = validate_right / float(validate_total)
     print("Epoch:{}, validate accuracy: {}".format(epoch, accuracy))
