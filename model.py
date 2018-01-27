688eed8ca3dbcda289d9fdd60838bea885368ce1
twinsnan0 <twinsnan@foxmail.com>
2018-01-27 23:55:33 +0800

    fix bug

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.resnet = models.resnet101(pretrained=True)
-        self.resnet.fc = F.relu(nn.Linear(2048, 1024))
+        self.resnet.fc = nn.Linear(2048, 1024)
         for param in self.resnet.parameters():
             param.requires_grad = True
         self.fc1 = nn.Linear(1024, 120)
